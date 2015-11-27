24c191cf371cbe1ad11a0e1c1f9bf3d92d2ae328
Surinder Gill <gillss4@mcmaster.ca>
2015-11-27 18:23:56 -0500

    add errythang2

--- js/spec/cookieHandler_test.js
+++ js/spec/cookieHandler_test.js
@@ -3,19 +3,16 @@ describe("Cookie Handler Unit Tests", function() {
   it("Set Cookie Name, Value and Expiry Days", function() {
   	setCookie("test",42, 999);
   	expect(setCookie.expires).not.toBeNull;
-  	console.log(setCookie.expires);
   });
 
   it("Cookie should not be set at 2", function() {
     var gC = getCookie("test");
-    console.log(gC);
-    expect(gC).not.toBe(2);
+    expect(gC).not.toEqual(2);
   });
 
   it("Get Cookie that was previously set at 42", function() {
     var gC = getCookie("test");
-    console.log(gC);
-    expect(gC).toBe('42');
+    expect(gC).toEqual(42);
   });
 
   it("Check if cookies are not Null", function() {
